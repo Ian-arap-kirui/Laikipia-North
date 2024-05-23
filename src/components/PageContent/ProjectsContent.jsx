61095c6628fa7625f6b934faf2a7322b13b36699
Ian-Mark <ianarapkirui@gmail.com>
2024-05-23 16:42:48 +0300

    single project and service  page added

--- src/components/PageContent/ProjectsContent.jsx
+++ src/components/PageContent/ProjectsContent.jsx
@@ -1,5 +1,52 @@
+import dummyImg from "../../assets/business.jpg";
 const ProjectsContent = () => {
-  return <div>ProjectsContent</div>;
+  return (
+    <>
+      <div className="blogImageContainer">
+        <h2>title</h2>
+        <img src={dummyImg} alt="blog-image" loading="lazy" />
+      </div>
+      <div className="blogDetailsContainer">
+        <div className="subSection">
+          <div>
+            <div class="details-text-area">
+              <h3 class="mt-0">Find Doctors And Medical Facilities</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt labore et dolore magna aliqua. Ut enim
+                ad minim veniam, quis nostrud exercitation ullamc laboris nisi
+                ut aliquip commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur commodo.
+              </p>
+              <h3>Health Benefits For Veterans</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt labore et dolore magna aliqua. Ut enim
+                ad minim veniam, quis nostrud exercitation ullamc laboris nisi
+                ut aliquip commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur commodo.
+              </p>
+              <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-12 mb-30"></div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-12 mb-30"></div>
+              </div>
+            </div>
+            <h3 class="mt-0">Help With Prescription Drug Costs</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamc laboris nisi ut
+              aliquip commodo consequat. Duis aute irure dolor in reprehenderit
+              in voluptate velit esse cillum dolore eu fugiat nulla pariatur
+              commodo.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProjectsContent;
