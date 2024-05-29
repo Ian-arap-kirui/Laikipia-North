051ece03876e9ea5a0164ac0059c260eb71e442e
Ian-Mark <ianarapkirui@gmail.com>
2024-05-29 11:16:54 +0300

    single page datatable quick fix

--- src/pages/Services/ServiceProposals.jsx
+++ src/pages/Services/ServiceProposals.jsx
@@ -4,7 +4,8 @@ const ServiceProposals = ({ services }) => {
   const proposals = services.flatMap((service) => service.proposals);
   const columns = [
     { field: "id", headerName: "ID", width: 90 },
-    { field: "filename", headerName: "Filename", flex: 1 },
+    { field: "filename", headerName: "Filename", width: 300 },
+    { field: "upload_date", headerName: "Uploaded on", flex: 1 },
     { field: "status", headerName: "Status", flex: 1 },
   ];
   return (
