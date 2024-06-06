5a7646e0c3f92222a3762618b5c8e36a55f53412
Ian-Mark <ianarapkirui@gmail.com>
2024-06-06 18:37:58 +0300

    project categs updates

--- src/utils/ServiceDetailsContainer.jsx
+++ src/utils/ServiceDetailsContainer.jsx
@@ -43,7 +43,7 @@ const ServiceDetailsContainer = ({ service }) => {
       {isConstituencyOfficeRoute && (
         <div className="serviceDetailsContainer">
           <h3>{service.title}</h3>
-          <h5>Categories the {service.title} deals with:</h5>
+          <h5>The {service.title} deals with:</h5>
           <ul>
             {categories.map((category, index) => (
               <Link
@@ -55,18 +55,33 @@ const ServiceDetailsContainer = ({ service }) => {
               </Link>
             ))}
           </ul>
-          <div className="servicefilteredProjects">
-            {filteredProjects.map((project) => (
-              <Link
-                to={`${location.pathname}/projects/${project.name}`}
-                key={project.id}
-              >
-                <h4>{project.name}</h4>
-                <b>{project.categ}</b>
-                <p>{project.description}</p>
-              </Link>
-            ))}
-          </div>
+          {selectedCategory ? (
+            <div className="servicefilteredProjects">
+              {filteredProjects.map((project) => (
+                <Link
+                  to={`${location.pathname}/projects/${project.name}`}
+                  key={project.id}
+                >
+                  <h4>{project.name}</h4>
+                  <b>{project.categ}</b>
+                  <p>{project.description}</p>
+                </Link>
+              ))}
+            </div>
+          ) : (
+            <>
+              <p>{service.description}</p>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d510690.01547941106!2d36.48533901891072!3d0.29413638592392544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x178635240419ec05%3A0x928ba7f70971d8de!2sLaikipia%20County!5e0!3m2!1sen!2ske!4v1716971153189!5m2!1sen!2ske"
+                width="100%"
+                height="450"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </>
+          )}
         </div>
       )}
 
