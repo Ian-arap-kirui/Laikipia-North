3d11285e94752a3599853abf56ae52e558d38dfb
Ian-Mark <ianarapkirui@gmail.com>
2024-06-06 18:27:48 +0300

    project categs added

--- src/utils/ServiceDetailsContainer.jsx
+++ src/utils/ServiceDetailsContainer.jsx
@@ -1,36 +1,81 @@
-import { Outlet, useParams, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { Outlet, useParams, useLocation, Link } from "react-router-dom";
 import "../components/PageContent/pageContentStyles/serviceContent.css";
+
+// Function to extract unique categories
+const getUniqueCategories = (projects) => {
+  const categories = projects.map((project) => project.categ);
+  return [...new Set(categories)];
+};
+
 const ServiceDetailsContainer = ({ service }) => {
   const { serviceId } = useParams();
   const location = useLocation();
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const categories = getUniqueCategories(service.projects);
 
-  // Check if the current route matches /services/:serviceId
   const isServiceRoute = location.pathname === `/services/${serviceId}`;
+  const isConstituencyOfficeRoute =
+    location.pathname === `/services/constituency-office`;
 
-  // Render the component with service description if it's a service route
-  if (isServiceRoute) {
-    return (
-      <div className="serviceDetailsContainer">
-        <h4>{service.title}</h4>
-        <p>{service.description}</p>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d510690.01547941106!2d36.48533901891072!3d0.29413638592392544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x178635240419ec05%3A0x928ba7f70971d8de!2sLaikipia%20County!5e0!3m2!1sen!2ske!4v1716971153189!5m2!1sen!2ske"
-          width="100%"
-          height="450"
-          style={{ border: 0 }}
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-        ></iframe>
-      </div>
-    );
-  }
+  const filteredProjects = selectedCategory
+    ? service.projects.filter((project) => project.categ === selectedCategory)
+    : [];
 
-  // Otherwise, render the Outlet to handle nested routes
   return (
     <>
-      <h4>{service.title}</h4>
-      <Outlet />
+      {isServiceRoute && !isConstituencyOfficeRoute && (
+        <div className="serviceDetailsContainer">
+          <h4>{service.title}</h4>
+          <p>{service.description}</p>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d510690.01547941106!2d36.48533901891072!3d0.29413638592392544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x178635240419ec05%3A0x928ba7f70971d8de!2sLaikipia%20County!5e0!3m2!1sen!2ske!4v1716971153189!5m2!1sen!2ske"
+            width="100%"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      )}
+
+      {isConstituencyOfficeRoute && (
+        <div className="serviceDetailsContainer">
+          <h3>{service.title}</h3>
+          <h5>Categories the {service.title} deals with:</h5>
+          <ul>
+            {categories.map((category, index) => (
+              <Link
+                to="#"
+                key={index}
+                onClick={() => setSelectedCategory(category)}
+              >
+                {category}
+              </Link>
+            ))}
+          </ul>
+          <div className="servicefilteredProjects">
+            {filteredProjects.map((project) => (
+              <Link
+                to={`${location.pathname}/projects/${project.name}`}
+                key={project.id}
+              >
+                <h4>{project.name}</h4>
+                <b>{project.categ}</b>
+                <p>{project.description}</p>
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
+
+      {!isServiceRoute && !isConstituencyOfficeRoute && (
+        <>
+          <h4>{service.title}</h4>
+          <Outlet />
+        </>
+      )}
     </>
   );
 };
