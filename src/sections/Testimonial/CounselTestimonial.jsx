488f869561e5cf1225e200995e4d60abe059bbb4
Ian-Mark <ianarapkirui@gmail.com>
2024-07-08 23:49:24 +0300

    responsive fixes 1

--- src/sections/Testimonial/CounselTestimonial.jsx
+++ src/sections/Testimonial/CounselTestimonial.jsx
@@ -23,8 +23,8 @@ const CounselTestimonial = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
+          slidesToShow: 3,
+          slidesToScroll: 3,
           infinite: true,
           dots: false,
         },
@@ -34,9 +34,14 @@ const CounselTestimonial = () => {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          initialSlide: 2,
-          infinite: true,
-          dots: false,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
         },
       },
     ],
